08c7779e7f0c73a8b41e3703927cb4f960aa81da
thaissbleal29 <thaisbleal2006@gmail.com>
2025-12-28 17:41:20 -0300

    toques finais

--- q1.R
+++ q1.R
@@ -16,14 +16,18 @@ plot(x, PMF,
      lwd = 2,
      xlab = "Número de clientes que pedem sobremesa",
      ylab = "P(X = x)",
-     main = "PMF - Distribuição Binomial (n = 50, p = 0.7)")
+     main = "")
+
+# Curva Normal de aproximação
+curve(dnorm(x, mean = valor_esperado, sd = desvio), add = TRUE, col = "red", lwd = 2)
+legend("topleft", legend = c("Binomial", "Normal"), col = c("black", "red"), lwd = 2)
 
 cdf = c(0,cumsum(PMF))
 cdf.plot = stepfun(x,cdf,f=0)
 par(mar=c(6,5,4,2))
 plot.stepfun(cdf.plot,col='blue',
              xlab=TeX('x'),ylab=TeX('F_X(x)'),
-             verticals=FALSE,main='Função de Distribuição Acumulada (CDF)',
+             verticals=FALSE,
              do.points=TRUE,
              pch=19, lwd=2,
              cex.lab=1.3, 
@@ -49,3 +53,25 @@ respostaC <- dbinom(31, 50, 0.7)
 cat("a) P(X ≥ 20) =", respostaA, "\n")
 cat("b) P(30 < X < 43) =", respostaB, "\n")
 cat("c) P(X = 31) =", respostaC, "\n")
+
+#6 p = 0.8
+PMF2 <- dbinom(x, size = n, prob = 0.8)
+plot(x, PMF2,
+     type = "h",
+     lwd = 2,
+     xlab = "Número de clientes que pedem sobremesa",
+     ylab = "P(X = x)")
+curve(dnorm(x, mean = 40, sd = sqrt(0.8*0.2*50)), add = TRUE, col = "red", lwd = 2)
+legend("topleft", legend = c("Binomial", "Normal"), col = c("black", "red"), lwd = 2)
+
+# n=100
+y <- 0:100
+PMF3 <- dbinom(y, size = 100, prob = 0.7)
+plot(y, PMF3,
+     type = "h",
+     lwd = 2,
+     xlab = "Número de clientes que pedem sobremesa",
+     ylab = "P(X = x)",
+     main = "")
+curve(dnorm(x, mean = 70, sd = sqrt(0.7*0.3*100)), add = TRUE, col = "red", lwd = 2)
+legend("topleft", legend = c("Binomial", "Normal"), col = c("black", "red"), lwd = 2)
